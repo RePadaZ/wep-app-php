3668e615cf833e1cedf0f5ec90d72c3baf9111ac
padaz <Ne-PadaZ@yandex.ru>
2024-11-21 22:17:25 +0300

    Add file project

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My web-site</title>
+    <link href="css/style.css" rel="stylesheet"/>
+    <link href="bootstrap-5.3.3-dist/css/bootstrap.css" rel="stylesheet"/>
+</head>
+    <body>
+        <div class="container-xxl border"> Content </div>
+    <script src="bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
